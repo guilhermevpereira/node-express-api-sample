f03ff289bb6b91f3f9ea1284134ab0751cece1e8
Guilherme P <guigavazpereira@gmail.com>
2020-07-09 19:50:06 -0300

    Implementa conexão com uma base de dados mysql

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,4 +4,8 @@ module.exports = app => {
     app.get("/tasks", (req, res) => {
         res.send("GET Request /tasks")        
     })
+
+    app.post("/tasks", (req, res) => {
+        res.send("POST Request /tasks")        
+    })
 }
